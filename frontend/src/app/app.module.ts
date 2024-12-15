e65929cefd256433eca11eedfffbb89249b5b473
Colin Groark <colin@nexleaf.org>
2024-12-15 16:56:06 +0000

    update package.json

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule,
   // NO_ERRORS_SCHEMA
 } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -10,21 +10,13 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
 import { ItemFormComponent } from './components/item-form/item-form.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ItemFormComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgSelectModule,
-    HttpClientModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  // schemas: [NO_ERRORS_SCHEMA]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ItemFormComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgSelectModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
