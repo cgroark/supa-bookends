30451a64431594abdd6df9b5dc3e4476dd905e8c
Colin Groark <colin@nexleaf.org>
2025-02-10 13:44:46 +0000

    build out connections view and fetch all

--- frontend/src/app/components/connections/connections.component.ts
+++ frontend/src/app/components/connections/connections.component.ts
@@ -4,7 +4,7 @@ import { jwtDecode}  from 'jwt-decode';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
-import  {Observable, of, ReplaySubject, Subject} from 'rxjs'
+import  {forkJoin, Observable, of, ReplaySubject, Subject} from 'rxjs'
 import {
   debounceTime,
   distinctUntilChanged,
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-connections',
@@ -28,8 +29,10 @@ import { NavbarComponent } from '../navbar/navbar.component';
 export class ConnectionsComponent implements OnInit {
   protected connections$: Observable<any[]>;
   protected connectionInput$ = new ReplaySubject<string>(1);
+  protected existingConnections: User[] = [];
   protected isLoading = false;
   protected isAdding = false;
+  protected isLoadingConnections = false;
   protected selectedConnection: any;
   protected userId: string = '';
   protected user: any;
@@ -60,12 +63,43 @@ export class ConnectionsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  const token = localStorage.getItem('auth_token_bookends');
+    const token = localStorage.getItem('auth_token_bookends');
     const decodedToken: any = token ? jwtDecode(token) : null;
     if(decodedToken) {
       const userId = decodedToken.sub;
       this.userId = userId;
+      this.fetchConnections(userId);
     }
+  }
+
+  protected fetchConnections(userId: string): void {
+    this.isLoadingConnections = true;
+    this.userService.getUserById(userId).subscribe({
+      next: (response) => {
+        console.warn("RES", response);
+        this.user = response;
+        if (this.user.connections?.length) {
+          const userRequests = this.user.connections.map((id: string) => this.userService.getUserById(id));
+          forkJoin<User[]>(userRequests).subscribe({
+            next: (users: User[]) => {
+              this.existingConnections = users;
+              console.warn('Fetched Connected Users:', this.existingConnections);
+              this.isLoadingConnections = false;
+            },
+            error: (error) => {
+              console.error('Error fetching connected users:', error);
+              this.isLoadingConnections = false;
+            }
+          });
+        } else {
+          this.isLoadingConnections = false;
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching user in backend:', error);
+        this.isLoadingConnections = false;
+      }
+    });
   }
 
   protected onChange(selected: any): void {
@@ -76,11 +110,12 @@ export class ConnectionsComponent implements OnInit {
   protected addConnection(id: string): void {
     console.warn('ID', id);
     this.isAdding = true;
-    const connections = {connections: [id]};
+    const connections = {connections: [...this.user.connections, id]};
     this.userService.updateUser(this.userId, connections).subscribe({
       next: (response) => {
         console.warn('res', response);
         this.isAdding = false;
+        this.fetchConnections(this.userId)
       },
       error: (error) => {
         console.error('Error updating user in backend:', error);
